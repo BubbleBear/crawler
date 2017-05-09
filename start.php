027e93235349ef6ceeccb97bdcb1a763b20c0966
Vatel <vesselvatel@gmail.com>
2017-05-09 14:07:06 +0800

    .

--- start.php
+++ start.php
@@ -6,14 +6,8 @@ $c['UrlMapSetDAO'] = function ($c) {
     return new \Vatel\DAO\UrlMapSetDAO($c);
 };
 
-// $c['Kernel']->crawl(array(
-//     'http://www.hdu.edu.cn/',
-// ));
+$c['Kernel']->crawl(array(
+    'http://www.hdu.edu.cn/',
+));
 
-$res = $c['HttpRequester']->doGetRequest('edusoho.com');
-
-// file_put_contents('tmp.html', $res);
-
-$links = $c['LinkExtractor']->extractLinks('http://news.cctv.com/2017/05/08/ARTIKNLkg8H28qj3RhKaXsB2170508.shtml', $res);
-
-var_dump($links);
+// exec('.\bin\phantomjs src\Scripts\test.js');
